41671eeecf9387d37f1ca8ede442c61c6c3bb900
miteskoladiya <miteshkoladiya1117@gmail.com>
2025-02-16 22:54:18 +0530

    finaal

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,12 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { CreateNewUser,GenerateNotes,GenerateStudyTypeContent,helloWorld } from "@/inngest/functions";
-
+export const runtime= 'edge';
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
+  streaming:'allow',
+  
   functions: [
     helloWorld, // <-- This is where you'll always add all your functions
     CreateNewUser,
